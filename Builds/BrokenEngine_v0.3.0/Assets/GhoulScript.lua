db785363fa84caec99f3b03d328b46d5874fa1ab
Jaumeavinyo <36154250+Jaumeavinyo@users.noreply.github.com>
2020-03-28 23:46:42 +0100

    compilling errors end engine crashers solved
    still in progress

--- Builds/BrokenEngine_v0.3.0/Assets/GhoulScript.lua
+++ Builds/BrokenEngine_v0.3.0/Assets/GhoulScript.lua
@@ -61,9 +61,9 @@ lua_table.currentState = State.IDL
 
 function HandleAggro()
 	ret = true 
-
+														
 	if currentTarget == 0 and Players() --sin objetivo inicial y existen players
-	then 
+	then	
 			if GeraltDistance < JaskierDistance
 			then
 				currentTarget = Geralt
@@ -87,44 +87,46 @@ function patrol()
 end
 
 local function Players() --function to know if there is a player in the area and where it is
-
+	
     ret = true
-	lua_table.SystemFunctions:LOG("YEAHHHH  players()")
+	lua_table.SystemFunctions:LOG("Players()?")
 
- --   if Geralt == 1 --Geralt comprovation
- --   then 
-   --     GeraltPos_x = lua_table.GameObjectFunctions:GetGameObjectPosX(Geralt)
-   --     GeraltPos_y = lua_table.GameObjectFunctions:GetGameObjectPosY(Geralt)
-   --     GeraltPos_z = lua_table.GameObjectFunctions:GetGameObjectPosZ(Geralt)
-		
- --   elseif Geralt == 0
---    then
-  --      lua_table.SystemFunctions:LOG("This Log was called from LUA table from a GhoulScript on HandleTheNearestPlayer function because Geralt is not found")     
-  --  end
+    if lua_table.Geralt ~= 0 --Geralt comprovation
+    then 
+		GeraltPos_x = lua_table.GameObjectFunctions:GetGameObjectPosX(lua_table.Geralt)
+		GeraltPos_y = lua_table.GameObjectFunctions:GetGameObjectPosY(lua_table.Geralt)
+		GeraltPos_z = lua_table.GameObjectFunctions:GetGameObjectPosZ(lua_table.Geralt)
+		lua_table.SystemFunctions:LOG("Geralt - YES")	
+    else--if  lua_table.Geralt == 0
+		lua_table.SystemFunctions:LOG("This Log was called from LUA table from a GhoulScript on Players()? function because Geralt is not found")
+		ret = false
+    end
 
- --   if Jaskier == 1 --Jaskier comprovation
- --   then 
-  --      JaskierPos_x = lua_table.GameObjectFunctions:GetGameObjectPosX(Jaskier)
- --       JaskierPos_y = lua_table.GameObjectFunctions:GetGameObjectPosY(Jaskier)
-  --      askierPos_z = lua_table.GameObjectFunctions:GetGameObjectPosZ(Jaskier)        
-  --  elseif Jaskier == 0 
-  --  then
-   --     lua_table.SystemFunctions:LOG("This Log was called from LUA table from a GhoulScript on HandleTheNearestPlayer function because Geralt is not found")
-  --  end
+    if  lua_table.Jaskier ~= 0 --Jaskier comprovation
+    then 
+        JaskierPos_x = lua_table.GameObjectFunctions:GetGameObjectPosX(lua_table.Jaskier)
+        JaskierPos_y = lua_table.GameObjectFunctions:GetGameObjectPosY(lua_table.Jaskier)
+        askierPos_z = lua_table.GameObjectFunctions:GetGameObjectPosZ(lua_table.Jaskier)   
+		lua_table.SystemFunctions:LOG("Jaskier - YES")	
+    else  
+        lua_table.SystemFunctions:LOG("This Log was called from LUA table from a GhoulScript on HandleTheNearestPlayer function because Geralt is not found")
+    end
 
     --calculate distances
 
-   -- JaskierDistance =  math.sqrt(JaskierPos_x ^ 2 + JaskierPos_z ^ 2)
-  --  GeraltDistance = math.sqrt(GrealtPos_x ^ 2 + GeraltPos_z ^ 2)
+    --lua_table.JaskierDistance =  math.sqrt(JaskierPos_x ^ 2 + JaskierPos_z ^ 2)
+    --lua_table.GeraltDistance = math.sqrt(GrealtPos_x ^ 2 + GeraltPos_z ^ 2)
 
     --calculate if necessary to change ghoul state to idl
 
-  --  if JaskierDistance ~= 0 and GeraltDistance ~= 0 -- HC as fuck
-  --  then
-  --      ret = true
---	else
+    --if lua_table.JaskierDistance >= 5 and lua_table.GeraltDistance >= 5 -- HC as fuck
+    --then
+	--	lua_table.SystemFunctions:LOG("both players under detection value to determine Players()? ret ")
+    --    ret = true
+	--else
 	--	lua_table.SystemFunctions:LOG("This Log was called from LUA table from a GhoulScript on HandleTheNearestPlayer function because Gerardo1 or jaskier1 are not in the area or where not detected")
-  --  end
+	--	lua_table.currentState = State.SEEK
+    --end
 
     return ret
 end
@@ -132,64 +134,60 @@ end
 function HandleIdleState() --handle if necessary to change idle state to patrol.
 	
     if lua_table.currentState == State.IDL
-    then
-		--lua_table.SystemFunctions:LOG("YEAHHHH  0")
+    then		
 		if lua_table.Stunned == false
 		then
-			lua_table.SystemFunctions:LOG("YEAHHHH  1")
 			if Players() == true
-			then
-				lua_table.SystemFunctions:LOG("YEAHHHH  2")
-				lua_table.currentState = State.PATROL --PETA ESTA LINEA
-				--lua_table.SystemFunctions:LOG("GhoulScript: New State: PATROL")
-			end 
-		--else if lua_table.Stunned == true -- when stunned is true idle state does not change. add a script to add stunned effect in enemy head when stunned
-		--then
-		--	Time = 0 --TODO timer
-			--timer for stunt here. dt??? when end Timer controller is false turn stunned to false
-		--		if Time == StuntTime 
-		--		then
+			then				
+			---	lua_table.currentState = State.PATROL 
+			--	lua_table.SystemFunctions:LOG("GhoulScript: New State: PATROL")		 
+			--elseif lua_table.Stunned == true -- when stunned is true idle state does not change. add a script to add stunned effect in enemy head when stunned
+			--then
+			--	local Time = 0 --TODO timer
+				--timer for stunt here. dt??? when end Timer controller is false turn stunned to false
+			--	if Time == StuntTime 
+			--	then
 			--		lua_table.Stunned = false
-		--		end
-			--end
+			--	end
+			end
 		end
     end
 end
 
 function HandlePatrolState() -- THIS IS NOT A PATROL ITSELF, IS JUST A LITTLE AREA IN WICH THE GHOULS WILL WALK. (little is like 3 meters in a circle)
 
-    if lua_table.currentState ==State.PATROL
-    then
-        Patrol()
-    end
+    --if lua_table.currentState ==State.PATROL
+   -- then
+        --Patrol()
+  --  end
     
-    JaskierPos_x = lua_table.GameObjectFunctions:GetGameObjectPosX(Jaskier)
-    JaskierPos_y = lua_table.GameObjectFunctions:GetGameObjectPosY(Jaskier)
-    JaskierPos_z = lua_table.GameObjectFunctions:GetGameObjectPosZ(Jaskier)
+    --JaskierPos_x = lua_table.GameObjectFunctions:GetGameObjectPosX(Jaskier)
+    --JaskierPos_y = lua_table.GameObjectFunctions:GetGameObjectPosY(Jaskier)
+    --JaskierPos_z = lua_table.GameObjectFunctions:GetGameObjectPosZ(Jaskier)
 
-    GeraltPos_x = lua_table.GameObjectFunctions:GetGameObjectPosX(Geralt)
-    GeraltPos_y = lua_table.GameObjectFunctions:GetGameObjectPosY(Geralt)
-    GeraltPos_z = lua_table.GameObjectFunctions:GetGameObjectPosZ(Geralt)
+    --GeraltPos_x = lua_table.GameObjectFunctions:GetGameObjectPosX(Geralt)
+    --GeraltPos_y = lua_table.GameObjectFunctions:GetGameObjectPosY(Geralt)
+    --GeraltPos_z = lua_table.GameObjectFunctions:GetGameObjectPosZ(Geralt)
 
-    JaskierDistance =  math.sqrt(JaskierPos_x ^ 2 + JaskierPos_z ^ 2)
-    GeraltDistance = math.sqrt(GeraltPos_x ^ 2 + GeraltPos_z ^ 2)
+    --JaskierDistance =  math.sqrt(JaskierPos_x ^ 2 + JaskierPos_z ^ 2)
+    --GeraltDistance = math.sqrt(GeraltPos_x ^ 2 + GeraltPos_z ^ 2)
 
 
-	if JaskierDistance ~= 0 and GeraltDistance ~= 0 -- HC as fuck
-    then
-        if HandleAggro()
-        then 
-			lua_table.currentState = State.SEEK
-			lua_table.SystemFunctions:LOG("GhoulScript: New State: SEEK")
-        end
-	else
-		lua_table.SystemFunctions:LOG("This Log was called from LUA table from a GhoulScript on HandleTheNearestPlayer function because Gerardo1 or jaskier1 are not in the area or where not detected")
-    end
+	--if JaskierDistance ~= 0 and GeraltDistance ~= 0 -- HC as fuck
+    --then
+     --   if HandleAggro()
+     --   then 
+	--		lua_table.currentState = State.SEEK
+	--		lua_table.SystemFunctions:LOG("GhoulScript: New State: SEEK")
+     --   end
+	--else
+	--	lua_table.SystemFunctions:LOG("This Log was called from LUA table from a GhoulScript on HandleTheNearestPlayer function because Gerardo1 or jaskier1 are not in the area or where not detected")
+    --end
 
-    if JaskierDistance < 7 or GeraltDistance < 7
-    then 
+   -- if JaskierDistance < 7 or GeraltDistance < 7
+   -- then 
          
-    end
+   -- end
 end
 
 function HandleAttackState()
@@ -212,18 +210,18 @@ end
 function lua_table:Awake()
    lua_table.SystemFunctions:LOG("This Log was called from LUA table from a GhoulScript on AWAKE")
 
-    -------------------- GET PLAYERS id START--------------------
-    Geralt = lua_table.GameObjectFunctions:FindGameObject("gerardo1")
-    Jaskier = lua_table.GameObjectFunctions:FindGameObject("jaskier1") 
+   -------------------- GET PLAYERS id START--------------------
+   lua_table.Geralt = lua_table.GameObjectFunctions:FindGameObject("gerardo1")
+   lua_table.Jaskier = lua_table.GameObjectFunctions:FindGameObject("jaskier1") 
 
-    if Geralt == 0 then lua_table.SystemFunctions:LOG ("A random GhoulScript: Null Geralt id, check the name of game object the script is looking for or add Geralt to the scene if not there already")
-    end
+   if lua_table.Geralt == 0 then lua_table.SystemFunctions:LOG ("A random GhoulScript: Null Geralt id, check the name of game object the script is looking for or add Geralt to the scene if not there already")
+   end
     
-    if Jaskier == 0 then lua_table.SystemFunctions:LOG ("A random GhoulScript: Null Jaskier id, check the name of game object the script is looking for or add Jaskier to the scene if not there already")
-    end
+   if lua_table.Jaskier == 0 then lua_table.SystemFunctions:LOG ("A random GhoulScript: Null Jaskier id, check the name of game object the script is looking for or add Jaskier to the scene if not there already")
+   end
 
-    lua_table.currentState = State.IDL
-    -------------------- GET PLAYERS id END --------------------
+   lua_table.currentState = State.IDL
+   -------------------- GET PLAYERS id END --------------------
 end
 
 function lua_table:Start()
