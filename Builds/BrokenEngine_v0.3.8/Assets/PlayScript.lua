7dcc2a1793e457d9957ac378a23af7f9825388a3
DavidTello1 <davidtellop@gmail.com>
2020-04-01 18:02:34 +0200

    Main Menu (except change scene)

---
+++ Builds/BrokenEngine_v0.3.8/Assets/PlayScript.lua
@@ -0,0 +1,15 @@
+function GetTablePlayScript()
+local lua_table = {}
+lua_table.System = Scripting.System()
+
+function lua_table:Awake()
+end
+
+function lua_table:Start()
+end
+
+function lua_table:Update()
+end
+
+return lua_table
+end
