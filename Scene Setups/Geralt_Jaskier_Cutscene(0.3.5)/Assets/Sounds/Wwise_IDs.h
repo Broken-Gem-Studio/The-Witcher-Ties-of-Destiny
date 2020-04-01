f2b7e16b35291355455c835e82a6a1a4de2f8921
sherzock <jacobo.galofre@gmail.com>
2020-04-01 17:21:31 +0200

    wwise error solving

--- Scene Setups/Geralt_Jaskier_Cutscene(0.3.5)/Assets/Sounds/Wwise_IDs.h
+++ Scene Setups/Geralt_Jaskier_Cutscene(0.3.5)/Assets/Sounds/Wwise_IDs.h
@@ -13,11 +13,37 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID BACKGROUNDMUSIC = 626769978U;
-        static const AkUniqueID FRANCESCO = 1153084749U;
-        static const AkUniqueID HELICOPTER = 2167849898U;
-        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID AARD_FX = 1051186882U;
+        static const AkUniqueID ATTACK_1_FX = 2754677910U;
+        static const AkUniqueID ATTACK_2_FX = 45795671U;
+        static const AkUniqueID ATTACK_3_FX = 1575009436U;
+        static const AkUniqueID ATTACK_4_FX = 3192225309U;
+        static const AkUniqueID ATTACK_5_FX = 1489897058U;
+        static const AkUniqueID DEATH_FX = 4255475438U;
+        static const AkUniqueID EVADE_FX = 1097011121U;
+        static const AkUniqueID LVL1_CONVERSATION_CUTSCENE = 4152204327U;
+        static const AkUniqueID POTION_FX = 2316188209U;
+        static const AkUniqueID RUN_FX = 3147007681U;
+        static const AkUniqueID SILVERFORMONSTERS_MUSIC = 2134787434U;
+        static const AkUniqueID SPIKEROOG_MUSIC = 530668550U;
+        static const AkUniqueID WALK_FX = 1003888003U;
     } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PLAYBACK_RATE = 1524500807U;
+        static const AkUniqueID RPM = 796049864U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
@@ -28,6 +54,7 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MOTION_FACTORY_BUS = 985987111U;
     } // namespace BUSSES
 
     namespace AUX_BUSSES
@@ -38,6 +65,7 @@ namespace AK
 
     namespace AUDIO_DEVICES
     {
+        static const AkUniqueID DEFAULT_MOTION_DEVICE = 4230635974U;
         static const AkUniqueID NO_OUTPUT = 2317455096U;
         static const AkUniqueID SYSTEM = 3859886410U;
     } // namespace AUDIO_DEVICES
