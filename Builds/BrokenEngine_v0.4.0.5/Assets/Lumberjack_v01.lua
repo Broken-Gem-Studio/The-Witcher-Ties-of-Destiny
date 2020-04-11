bd342aec7d0de54e96b611a48fe708c4b3380669
Jaumeavinyo <36154250+Jaumeavinyo@users.noreply.github.com>
2020-04-12 00:57:17 +0200

    seek donete

--- Builds/BrokenEngine_v0.4.0.5/Assets/Lumberjack_v01.lua
+++ Builds/BrokenEngine_v0.4.0.5/Assets/Lumberjack_v01.lua
@@ -46,6 +46,9 @@ local MyUID = 0
 local Geralt = 0
 local Jaskier = 0
 
+local Nvec3x = 0
+local Nvec3z = 0  -->Movement
+local Nvec3y = 0
 
 ---HandlePRE_DETECTION()
 local SpawnPosX = 0
@@ -64,12 +67,18 @@ local LookLeftRight_TimeController = 0
 local CurrentPatrolTargetX = 0
 local CurrentPatrolTargetY = 0
 local Arrived2PatrolTarget = true
-local PatrolSpeed = 15
-local Nvec3x = 0
-local Nvec3z = 0
-local Nvec3y = 0
+local PatrolSpeed = 20
 
-local distance2PatrolTarget = 0
+
+local DistanceMagnitude = 0
+
+
+---HandleAggro()
+local GeraltDistance = 0
+local JaskierDistance = 0
+
+--Players()
+
 
 -----------------------------------------------------------------------------------------
 -- Inspector Variables
@@ -83,7 +92,7 @@ lua_table.collider_effect = 0
 
 lua_table.MaxHealth = 100
 lua_table.CurrentHealth = 0
-lua_table.MaxSpeed = 150
+lua_table.MaxSpeed = 100
 lua_table.CurrentTarget = 0
 
 lua_table.AggroDistance = 150
@@ -93,6 +102,9 @@ lua_table.CurrentSubState = SubState.NONE
 lua_table.CurrentAnim = Anim.NONE
 
 lua_table.Pos = 0
+
+lua_table.MinDistanceFromPlayer = 20
+
 -----------------------------------------------------------------------------------------
 -- EXTERNAL FUNCTIONS
 -----------------------------------------------------------------------------------------
@@ -103,6 +115,33 @@ end
 -----------------------------------------------------------------------------------------
 -- SUB FUNCTIONS
 -----------------------------------------------------------------------------------------
+
+function ApplyVelocity( )
+
+	if lua_table.CurrentState == State.PRE_DETECTION and lua_table.CurrentSubState == PATROL
+	then 
+		if DistanceMagnitude > 0.2 --do this because if not then lumberjack shakes
+		then
+			Nvec3x = Nvec3x*PatrolSpeed
+			Nvec3z = Nvec3z*PatrolSpeed
+		else
+			Nvec3x = 0
+			Nvec3y = 0
+			Arrived2PatrolTarget = true
+		end
+	elseif lua_table.CurrentState == State.SEEK
+	then
+		if DistanceMagnitude > lua_table.MinDistanceFromPlayer 
+		then
+			Nvec3x = Nvec3x*lua_table.MaxSpeed
+			Nvec3z = Nvec3z*lua_table.MaxSpeed
+		else
+			Nvec3x = 0
+			Nvec3y = 0
+			Arrived2PatrolTarget = true
+		end
+	end
+end
 
 function seekPatrolTarget(CurrentPatrolTargetX,CurrentPatrolTargetZ)	 	
 	
@@ -116,18 +155,75 @@ function seekPatrolTarget(CurrentPatrolTargetX,CurrentPatrolTargetZ)
 	
 	Nvec3x = vec3x / DistanceMagnitude
 	Nvec3z = vec3z / DistanceMagnitude -- Normalized values
-	
-	if DistanceMagnitude > 0.2 --do this because if not then lumberjack shakes
-	then
-		Nvec3x = Nvec3x*PatrolSpeed
-		Nvec3z = Nvec3z*PatrolSpeed
-	else
-		Nvec3x = 0
-		Nvec3y = 0
-		Arrived2PatrolTarget = true
-	end
 end
 
+function seekTarget()	 	
+	
+	CurrentTargetPos = lua_table.TransformFunctions:GetPosition(lua_table.CurrentTarget)
+
+	vec3x = CurrentTargetPos[1] - lua_table.Pos[1]
+	vec3z = CurrentTargetPos[3] - lua_table.Pos[3]  -- Direction
+	
+	vec3xpow = vec3x * vec3x
+	vec3zpow = vec3z * vec3z -- pre calculus
+	
+	DistanceMagnitude = math.sqrt( vec3xpow + vec3zpow) --y not used
+	
+	Nvec3x = vec3x / DistanceMagnitude
+	Nvec3z = vec3z / DistanceMagnitude -- Normalized values
+end
+
+function Players()
+	ret = true
+
+	if Geralt ~= 0
+	then
+		GeraltPos = lua_table.TransformFunctions:GetPosition(Geralt)
+	else 
+		lua_table.SystemFunctions:LOG("This Log was called from a Lumberjack on Players() function because Geralt is not found")
+		ret = false
+	end
+
+	if Jaskier ~= 0
+	then
+		JaskierPos = lua_table.TransformFunctions:GetPosition(Jaskier)	
+	else 
+		lua_table.SystemFunctions:LOG("This Log was called from a Lumberjack on Players() function because Jaskier is not found")
+		ret = false
+	end
+	
+	--calculate distances
+	Aj = JaskierPos[1] - lua_table.Pos[1]	
+	Bj = JaskierPos[3] - lua_table.Pos[3]
+	JaskierDistance = math.sqrt(Aj^2+Bj^2)
+
+	Ag = GeraltPos[1] - lua_table.Pos[1]	
+	Bg = GeraltPos[3] - lua_table.Pos[3]
+	GeraltDistance = math.sqrt(Ag^2+Bg^2)
+
+	return ret
+end
+
+function HandleAggro()
+	ret = true
+	if lua_table.CurrentTarget == 0 and Players() == true --sin objetivo inicial y existen players
+	then	
+			if GeraltDistance < lua_table.AggroDistance  
+			then
+				lua_table.CurrentTarget = Geralt
+				lua_table.SystemFunctions:LOG("GERALT IN AGGRO")
+				return true			
+			elseif JaskierDistance < lua_table.AggroDistance  
+			then
+				lua_table.CurrentTarget = Jaskier
+				lua_table.SystemFunctions:LOG("JASKIER IN AGGRO")
+			else
+				--lua_table.SystemFunctions:LOG("NO PLAYERS INSIDE AGGRO DISTANCE")
+				return false
+			end	
+	end --TODO when a current target dies need to change current atrget for a new one
+	return ret
+end
 
 -----------------------------------------------------------------------------------------
 -- MAIN FUNCTIONS
@@ -140,7 +236,7 @@ function HandlePRE_DETECTION()
 		---
 		if isSelectedSubState == false ---Will enter 1 time to decide SubState
 		then
-			SelectedSubState = lua_table.SystemFunctions:RandomNumberInRange(0,4)
+			SelectedSubState = lua_table.SystemFunctions:RandomNumberInRange(0,10)
 			if SelectedSubState <= 10 and SelectedSubState >= 5
 			then
 				lua_table.CurrentSubState = SubState.IDL
@@ -171,39 +267,49 @@ function HandlePRE_DETECTION()
 			end
 		elseif lua_table.CurrentSubState == SubState.PATROL ---Will enter every frame if true
 		then
-		--lua_table.SystemFunctions:LOG("SUB STATE PATROL")
-			--if Aggro() == false
-			--then
-				if Arrived2PatrolTarget == true
+			if Arrived2PatrolTarget == true
+			then
+				--choose random target
+				if LookLeftRight_AnimController == false
 				then
-					--choose random target
-					if LookLeftRight_AnimController == false
-					then
-						lua_table.AnimationSystem:PlayAnimation(Anim.ATTACK1,30)
-						LookLeftRight_AnimController = true
-						LookLeftRight_TimeController = PerfGameTime()
-					end
-					Timer = PerfGameTime()
-					if Timer - LookLeftRight_TimeController > 5000
-					then
-						CurrentPatrolTargetX  = lua_table.SystemFunctions:RandomNumberInRange(SpawnPosX-30,SpawnPosX+30)
-						CurrentPatrolTargetZ = lua_table.SystemFunctions:RandomNumberInRange(SpawnPosZ-30,SpawnPosZ+30)
-						Arrived2PatrolTarget = false
-					end
-
-				elseif Arrived2PatrolTarget == false
+					lua_table.AnimationSystem:PlayAnimation(Anim.ATTACK1,30)
+					LookLeftRight_AnimController = true
+					LookLeftRight_TimeController = PerfGameTime()
+				end
+				Timer = PerfGameTime()
+				if Timer - LookLeftRight_TimeController > 5000 --TODO change value for a animation duration
 				then
-					seekPatrolTarget(CurrentPatrolTargetX,CurrentPatrolTargetZ)
-					LookLeftRight_AnimController = false
-				end	
+					CurrentPatrolTargetX  = lua_table.SystemFunctions:RandomNumberInRange(SpawnPosX-30,SpawnPosX+30)
+					CurrentPatrolTargetZ = lua_table.SystemFunctions:RandomNumberInRange(SpawnPosZ-30,SpawnPosZ+30)
+					Arrived2PatrolTarget = false
+				end
+			elseif Arrived2PatrolTarget == false
+			then
+				seekPatrolTarget(CurrentPatrolTargetX,CurrentPatrolTargetZ)
+				LookLeftRight_AnimController = false
+			end	
 			--end
 		end
 		---
+		PlayerSeen = HandleAggro()
+		---
+	elseif PlayerSeen == true
+	then
+		lua_table.CurrentState = State.SEEK
+		lua_table.CurrentSubState = Substate.NONE
+		
 	end
-
 end
 
+function HandleSEEK()
+	
+	seekTarget()
 
+	if DistanceMagnitude <= MinDistanceFromPlayer
+	then
+		CurrentState = State.ATTACK
+	end
+end
 -----------------------------------------------------------------------------------------
 -- Main Code
 -----------------------------------------------------------------------------------------
@@ -239,7 +345,6 @@ function lua_table:Awake()
 	SpawnPosX = lua_table.TransformFunctions:GetPositionX()
 	SpawnPosY = lua_table.TransformFunctions:GetPositionY()
 	SpawnPosZ = lua_table.TransformFunctions:GetPositionZ()
-
 end
 
 function lua_table:Start()
@@ -255,6 +360,8 @@ end
 
 function lua_table:Update()
 
+	lua_table.Pos = lua_table.TransformFunctions:GetPosition(MyUID)
+
 	if lua_table.CurrentHealth <= 1 
 	then
 		lua_table.CurrentState = State.DEATH
@@ -262,13 +369,15 @@ function lua_table:Update()
 
 	if lua_table.CurrentState == State.PRE_DETECTION
 	then
-		HandlePRE_DETECTION()
-		
+		HandlePRE_DETECTION()	
+	elseif lua_table.CurrentState == State.SEEK
+	then
+		HandleSEEK()
 	end
 
-	lua_table.Pos = lua_table.TransformFunctions:GetPosition(MyUID)
+	ApplyVelocity() --decides if move function will move or not in x and z axis
 	lua_table.PhysicsSystem:Move(Nvec3x,Nvec3z,MyUID)
-	lua_table.TransformFunctions:LookAt(lua_table.Pos[1] + Nvec3x,lua_table.Pos[2],lua_table.Pos[3] + Nvec3z,true)
+	lua_table.TransformFunctions:LookAt(lua_table.Pos[1] + Nvec3x,lua_table.Pos[2],lua_table.Pos[3] + Nvec3z,MyUID) -- PROVISIONAL, QUEDA MUY ARTIFICIAL
 
 end
 
