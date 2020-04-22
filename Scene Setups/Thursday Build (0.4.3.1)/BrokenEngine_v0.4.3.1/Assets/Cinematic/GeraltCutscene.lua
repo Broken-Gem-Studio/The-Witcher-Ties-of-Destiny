fbc1717c71aebdd761eb5448b76c660d05db382b
Josep Lleal <36154137+JosepLleal@users.noreply.github.com>
2020-04-22 16:27:28 +0200

    Cinematic scene updated

--- Scene Setups/Thursday Build (0.4.3.1)/BrokenEngine_v0.4.3.1/Assets/Cinematic/GeraltCutscene.lua
+++ Scene Setups/Thursday Build (0.4.3.1)/BrokenEngine_v0.4.3.1/Assets/Cinematic/GeraltCutscene.lua
@@ -7,13 +7,15 @@ lua_table.AnimationFunctions = Scripting.Animations()
 -- Camera target GO names
 lua_table.geralt_GO = "Geralt"
 lua_table.speed = 30.0
+local Geralt_UID = 0
 
 function lua_table:Awake()
     lua_table.System:LOG ("This Log was called from GeraltCutscene on AWAKE")
 end
 
 function lua_table:Start()
-    lua_table.AnimationFunctions:PlayAnimation("Cutscene", lua_table.speed)
+    Geralt_UID = lua_table.GameObjectFunctions:GetMyUID()
+    lua_table.AnimationFunctions:PlayAnimation("Cutscene", lua_table.speed, Geralt_UID)
 end
 
 function lua_table:Update()
