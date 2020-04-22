e0256fc821a0b0ed5eccdb9ec183a5642607a71d
Dani Lorenzo <dlorenzolaguno17@gmail.com>
2020-04-22 17:30:00 +0200

    Changed camera and button values

--- Scene Setups/Thursday Build (0.4.3.1)/BrokenEngine_v0.4.3.1/Assets/MainMenu/SignScript.lua
+++ Scene Setups/Thursday Build (0.4.3.1)/BrokenEngine_v0.4.3.1/Assets/MainMenu/SignScript.lua
@@ -9,10 +9,12 @@ local startButton = 0
 local fallingTime = 7.4
 local lastTimeFallen = 0
 local notPlayedIdle = true
+local MyUUID = 0
 
 function lua_table:Awake()
+	MyUUID = lua_table.ObjectFunctions:GetMyUID()
 	startButton = lua_table.ObjectFunctions:FindGameObject("StartButton")
-	lua_table.AnimationFunctions:PlayAnimation("Fall", 30)
+	lua_table.AnimationFunctions:PlayAnimation("Fall", 30, MyUUID)
 	lastTimeFallen = lua_table.SystemFunctions:GameTime()
 end
 
@@ -22,7 +24,7 @@ end
 function lua_table:Update()
 	if lua_table.SystemFunctions:GameTime() > lastTimeFallen + fallingTime and notPlayedIdle
 	then
-		lua_table.AnimationFunctions:PlayAnimation("Idle", 30)
+		lua_table.AnimationFunctions:PlayAnimation("Idle", 30, MyUUID)
 		notPlayedIdle = false
 	lua_table.InterfaceFunctions:MakeElementVisible("Button", startButton)
 	end
