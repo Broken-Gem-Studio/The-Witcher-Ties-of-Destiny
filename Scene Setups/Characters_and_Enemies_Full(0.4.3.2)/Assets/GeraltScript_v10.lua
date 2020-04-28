983a4832b32e0bb8fa096b24f96bc77f5fed1df5
Carles Homs <36209557+ch0m5@users.noreply.github.com>
2020-04-28 16:59:53 +0200

    Commented Stun Audio Geralt

--- Scene Setups/Characters_and_Enemies_Full(0.4.3.2)/Assets/GeraltScript_v10.lua
+++ Scene Setups/Characters_and_Enemies_Full(0.4.3.2)/Assets/GeraltScript_v10.lua
@@ -1460,7 +1460,7 @@ function lua_table:OnTriggerEnter()
 			if enemy_script.collider_effect == attack_effects_ID.stun
 			then
 				lua_table.AnimationFunctions:PlayAnimation("get_up", 50.0, my_GO_UID)
-				lua_table.AudioFunctions:PlayAudioEvent("Play_Geralt_getting_up")	--TODO-Audio:
+				--lua_table.AudioFunctions:PlayAudioEvent("Play_Geralt_getting_up")	--TODO-Audio:
 
 				AttackColliderShutdown()
 				ParticlesShutdown(false)
