1b9141bdae756c76e006fbe7ae9982178af80f79
Akarumbra <32853966+Akarumbra@users.noreply.github.com>
2020-05-02 18:18:51 +0200

    Audio issue solved

--- Scene Setups/Official Buildv0.5.3 - main menu audio/BrokenEngine_v0.5.3/Assets/MainMenu/SignScript.lua
+++ Scene Setups/Official Buildv0.5.3 - main menu audio/BrokenEngine_v0.5.3/Assets/MainMenu/SignScript.lua
@@ -4,6 +4,7 @@ lua_table.SystemFunctions = Scripting.System()
 lua_table.AnimationFunctions = Scripting.Animations()
 lua_table.ObjectFunctions = Scripting.GameObject()
 lua_table.InterfaceFunctions = Scripting.Interface()
+lua_table.Audio = Scripting.Audio()
 
 local startButton = 0
 local fallingTime = 7.4
@@ -15,6 +16,7 @@ function lua_table:Awake()
 	MyUUID = lua_table.ObjectFunctions:GetMyUID()
 	startButton = lua_table.ObjectFunctions:FindGameObject("StartButton")
 	lua_table.AnimationFunctions:PlayAnimation("Fall", 30, MyUUID)
+	lua_table.Audio:PlayAudioEvent("Play_menu_2")
 	lastTimeFallen = lua_table.SystemFunctions:GameTime()
 end
 
