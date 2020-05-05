f8ec838aa6ae820a6bf9dd922b2c9260bd11b9b7
Carles Homs <36209557+ch0m5@users.noreply.github.com>
2020-05-05 18:36:51 +0200

    Music Manager

--- Scene Setups/Characters_New_FBXs(0.5.4.3)/Assets/Music_Manager.lua
+++ Scene Setups/Characters_New_FBXs(0.5.4.3)/Assets/Music_Manager.lua
@@ -1,76 +1,81 @@
 function GetTableMusicTrigger()
 local lua_table = {}
-lua_table.System = Scripting.System()
-lua_table.Physic = Scripting.Physics()
-lua_table.Audios = Scripting.Audio()
-lua_table.GO = Scripting.GameObject()
+lua_table.SystemFunctions = Scripting.System()
+lua_table.AudioFunctions = Scripting.Audio()
+lua_table.GameObjectFunctions = Scripting.GameObject()
 
-local UID = 0
-lua_table.camera = "MainCamera"
-lua_table.type = {DEFAULT = 1, COMBAT = 2, REST = 3, MENU = 4}
-lua_table.currenttype = 1
-local currentmusic = 0
-local UIDCamera = 0
+-- GOs
+local camera_GO_UID	--If we decide to make it so battle music sounds when inside the frustum, currently does nothing
+
+local geralt_GO_UID
+local geralt_script
+
+local jaskier_GO_UID
+local jaskier_script
+
+-- Variables
+local music_types = {
+	no_request = -1
+	
+	none = 0,
+	default = 1,
+	combat = 2,
+	rest = 3,
+	menu = 4
+}
+lua_table.requested_music = no_request
+lua_table.current_music = 0
 
 function lua_table:Awake()
+	camera_GO_UID = lua_table.GameObjectFunctions:FindGameObject("Camera")
+	geralt_GO_UID = lua_table.GameObjectFunctions:FindGameObject("Geralt")
+	jaskier_GO_UID = lua_table.GameObjectFunctions:FindGameObject("Jaskier")
+
+	if geralt_GO_UID ~= 0 then geralt_script = lua_table.GameObjectFunctions:GetScript(geralt_GO_UID) end
+	if jaskier_GO_UID ~= 0 then jaskier_script = lua_table.GameObjectFunctions:GetScript(jaskier_GO_UID) end
 end
 
 function lua_table:Start()
-
-	UID = lua_table.GO:GetMyUID()
-
-	UIDCamera = lua_table.GO:FindGameObject(lua_table.camera)
-
-	BGMPlaying = false --some background music is playing
-	lua_table.Audios:PlayAudioEvent("Main_Menu")
-
+	
 end
 
 function lua_table:Update()
-
-end
-
-function lua_table:OnTriggerEnter()
-	local entity = lua_table.Physic:OnTriggerEnter(UID)
-	local layer = lua_table.GO:GetLayerByID(entity)
-	lua_table.System:LOG(layer)
-	if layer == 1
+	
+	-- Decide Music
+	if geralt_script.enemies_nearby or jaskier_script.enemies_nearby
 	then
-		if lua_table.currenttype == 1 and currentmusic ~= 1 and lua_table.enemies_nearby == false
+		if lua_table.current_music ~= music_types.combat then lua_table.requested_music = music_types.combat end
+	else
+		if lua_table.current_music ~= music_types.default then lua_table.requested_music = music_types.default end
+	end
+
+	-- Apply Music if needed
+	if lua_table.requested_music > -1
+	then
+		if lua_table.requested_music = music_types.none
 		then
-			lua_table.System:LOG("Default")
-			currentmusic = 1
-			lua_table.Audios:StopAudioEvent("Play_Main_menu", UIDCamera)
-			lua_table.Audios:StopAudioEvent("Play_Silver_for_Monsters_loop", UIDCamera)
-			lua_table.Audios:StopAudioEvent("Rest", UIDCamera)
-			lua_table.Audios:PlayAudioEvent("Play_Trial_of_the_Grasses_loop", UIDCamera)
-		end
-		if lua_table.currenttype == 2 and currentmusic ~= 2 and lua_table.enemies_nearby
-		then
-			lua_table.System:LOG("Combat")
-			currentmusic = 2
-			lua_table.Audios:StopAudioEvent("Play_Main_menu", UIDCamera)
-			lua_table.Audios:PlayAudioEvent("Play_Silver_for_Monsters_loop", UIDCamera)
-			lua_table.Audios:StopAudioEvent("Rest", UIDCamera)
-			lua_table.Audios:StopAudioEvent("Play_Trial_of_the_Grasses_loop", UIDCamera)
-		end
-		if lua_table.currenttype == 3 and currentmusic ~= 3
-		then
-			lua_table.System:LOG("Rest")
-			currentmusic = 3
-			lua_table.Audios:StopAudioEvent("Play_Main_menu", UIDCamera)
-			lua_table.Audios:StopAudioEvent("Play_Silver_for_Monsters_loop", UIDCamera)
-			lua_table.Audios:PlayAudioEvent("Rest", UIDCamera)
-			lua_table.Audios:StopAudioEvent("Play_Trial_of_the_Grasses_loop", UIDCamera)
-		end
-		if lua_table.currenttype == 4 and currentmusic ~= 4
-		then
-			lua_table.System:LOG("Menu")
-			currentmusic = 4
-			lua_table.Audios:PlayAudioEvent("Play_Main_menu", UIDCamera)
-			lua_table.Audios:StopAudioEvent("Play_Silver_for_Monsters_loop", UIDCamera)
-			lua_table.Audios:StopAudioEvent("Rest", UIDCamera)
-			lua_table.Audios:StopAudioEvent("Play_Trial_of_the_Grasses_loop", UIDCamera)
+			--lua_table.SystemFunctions:LOG("None")
+			lua_table.current_music = 0
+			lua_table.AudioFunctions:StopAudioEvent("Play_Main_menu")
+			lua_table.AudioFunctions:StopAudioEvent("Play_Silver_for_Monsters_loop")
+			lua_table.AudioFunctions:StopAudioEvent("Rest")
+			lua_table.AudioFunctions:StopAudioEvent("Play_Trial_of_the_Grasses_loop")
+
+		elseif lua_table.requested_music = music_types.default
+			--lua_table.SystemFunctions:LOG("Default")
+			lua_table.current_music = 1
+			lua_table.AudioFunctions:StopAudioEvent("Play_Main_menu", UIDCamera)
+			lua_table.AudioFunctions:StopAudioEvent("Play_Silver_for_Monsters_loop", UIDCamera)
+			lua_table.AudioFunctions:StopAudioEvent("Rest", UIDCamera)
+			lua_table.AudioFunctions:PlayAudioEvent("Play_Trial_of_the_Grasses_loop", UIDCamera)
+
+		elseif lua_table.requested_music = music_types.combat
+			--lua_table.SystemFunctions:LOG("Combat")
+			lua_table.current_music = 2
+			lua_table.AudioFunctions:StopAudioEvent("Play_Main_menu")
+			lua_table.AudioFunctions:PlayAudioEvent("Play_Silver_for_Monsters_loop")
+			lua_table.AudioFunctions:StopAudioEvent("Rest")
+			lua_table.AudioFunctions:StopAudioEvent("Play_Trial_of_the_Grasses_loop")
 		end
 	end
 end
