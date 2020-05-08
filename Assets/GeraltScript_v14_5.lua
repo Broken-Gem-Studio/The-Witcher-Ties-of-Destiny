3db52cab5ef7b91c0fea0948b6faa32416721fb7
Carles Homs <36209557+ch0m5@users.noreply.github.com>
2020-05-08 12:01:40 +0200

    Added Potion Particle GO on Characters SetupScene

--- Assets/GeraltScript_v14_5.lua
+++ Assets/GeraltScript_v14_5.lua
@@ -1,4 +1,4 @@
-function	GetTableGeraltScript_v14()
+function	GetTableGeraltScript_v14_5()
 local lua_table = {}
 lua_table.SystemFunctions = Scripting.System()
 lua_table.TransformFunctions = Scripting.Transform()
