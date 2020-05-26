522b3a62d542f6fe18dcbffff95b15e1d1681be0
Josep Lleal <36154137+JosepLleal@users.noreply.github.com>
2020-05-26 20:13:15 +0200

    ForestCinematicRework

--- Scene Setups/Official Build v0.6.4/Assets/Cinematic/CinematicCameraScript.lua
+++ Scene Setups/Official Build v0.6.4/Assets/Cinematic/CinematicCameraScript.lua
@@ -13,6 +13,7 @@ lua_table.cube = "Cube"
 lua_table.value_ = 0
 lua_table.scene_uid = 0
 lua_table.skip_threshold = 0
+local Fade = 0
 
 -- Camera position
 local offset = {}
@@ -27,6 +28,10 @@ local time = 0
 local started_time = 0
 local start_motion_time = 0
 
+-- Camera Positions
+local CurrentCameraPos = {}
+local Pos_Minions = {508.406, 57.469, -204.635}
+
 -- Cinematic conditions 
 local follow_jaskier = false
 local camera_panning = true
@@ -38,12 +43,51 @@ local music_played = false
 local cube_moved = false
 local next_scene = true
 
-function Lerp(start, end_, value)
+local function Lerp(start, end_, value)
+
     if value > 1.0
     then
         value = 1.0
     end
+
     return (1 - value) * start + value * end_
+end
+
+local function GoTo(position, speed)
+
+    speed = 1000 / speed
+    value = time / speed
+
+    local pos = lua_table.Transform:GetPosition(lua_table.GameObjectFunctions:GetMyUID())
+
+    local x = Lerp(pos[1], position[1], value)
+    local y = Lerp(pos[2], position[2], value)
+    local z = Lerp(pos[3], position[3], value)
+
+    lua_table.Transform:SetPosition(x, y, z, lua_table.GameObjectFunctions:GetMyUID())
+
+end
+
+local function SkipButton()
+
+    -- Skip Scene code
+    if lua_table.skip_threshold <= 0.00 then
+       lua_table.skip_threshold = 0.00
+   end
+
+   if lua_table.Input:IsGamepadButton(1, "BUTTON_A", "REPEAT") and next_scene == true then
+       lua_table.skip_threshold = lua_table.skip_threshold + 0.4
+   else 
+       lua_table.skip_threshold = lua_table.skip_threshold - 0.6
+   end
+
+   if lua_table.skip_threshold >= 100 and next_scene == true then
+       lua_table.Scene:LoadScene(lua_table.scene_uid)
+       next_scene = false
+   end
+
+   lua_table.UI:SetUICircularBarPercentage(lua_table.skip_threshold, BarID)
+
 end
 
 function lua_table:Awake()
@@ -52,98 +96,87 @@ end
 
 function lua_table:Start()
     -- Camera initial position (Players unseen)
-    lua_table.Transform:SetPosition(-970, 120, -4450, lua_table.GameObjectFunctions:GetMyUID())
+    lua_table.Transform:SetPosition(424.949, 57.469, -266.669, lua_table.GameObjectFunctions:GetMyUID())
+    lua_table.Transform:SetObjectRotation(132.264, 38.413, -147.605, lua_table.GameObjectFunctions:GetMyUID())
 
     Cube_ID = lua_table.GameObjectFunctions:FindGameObject(lua_table.cube)
     BarID = lua_table.GameObjectFunctions:FindGameObject("SkipBarForest")
+    Fade = lua_table.GameObjectFunctions:FindGameObject("Fade")
 
     started_time = lua_table.System:GameTime()
 end
 
+
 function lua_table:Update()
     time = lua_table.System:GameTime() - started_time
+    CurrentCameraPos = lua_table.Transform:GetPosition(lua_table.GameObjectFunctions:GetMyUID())
 
-    -- Skip Scene code
-    if lua_table.skip_threshold <= 0.00 then
-        lua_table.skip_threshold = 0.00
-    end
+    SkipButton()
 
-    if lua_table.Input:IsGamepadButton(1, "BUTTON_A", "REPEAT") and next_scene == true then
-        lua_table.skip_threshold = lua_table.skip_threshold + 0.4
-    else 
-        lua_table.skip_threshold = lua_table.skip_threshold - 0.6
-    end
-
-    if lua_table.skip_threshold >= 100 and next_scene == true then
-        lua_table.Scene:LoadScene(lua_table.scene_uid)
-		next_scene = false
-    end
-
-    lua_table.UI:SetUICircularBarPercentage(lua_table.skip_threshold, BarID)
-
-    
-    if camera_panning -- Camera pans downwards
+    if camera_panning -- Camera pans sideways
     then
-        local value_ = time / 3
-        
-        local position = lua_table.Transform:GetPosition(lua_table.GameObjectFunctions:GetMyUID())
-        position[2] = Lerp(120, 24, value_)
+        GoTo(Pos_Minions, 1)
 
-        lua_table.Transform:SetPosition(position[1], position[2], position[3], lua_table.GameObjectFunctions:GetMyUID())
-        if value_ >= 1
+        local value = time / 4
+        local alpha = Lerp(1, 0, value)
+        lua_table.UI:ChangeUIComponentColor("Image",0,0,0, alpha, Fade)
+
+
+
+        if CurrentCameraPos[1] == Pos_Minions[1]
         then
-            local pos = lua_table.Transform:GetPosition(lua_table.GameObjectFunctions:GetMyUID()) 
-            local cube_pos = lua_table.Transform:GetPosition(Cube_ID)
+            -- local pos = lua_table.Transform:GetPosition(lua_table.GameObjectFunctions:GetMyUID()) 
+            -- local cube_pos = lua_table.Transform:GetPosition(Cube_ID)
 
-            offset[1] = pos[1] - cube_pos[1]
-            offset[2] = pos[2] - cube_pos[2]
-            offset[3] = pos[3] - cube_pos[3]
+            -- offset[1] = pos[1] - cube_pos[1]
+            -- offset[2] = pos[2] - cube_pos[2]
+            -- offset[3] = pos[3] - cube_pos[3]
 
             camera_panning = false
         end
     end
     
-    if not camera_panning -- Follow Cube
-    then 
-        local new_pos = {}
-        local cube_pos = lua_table.Transform:GetPosition(Cube_ID)
+    -- if not camera_panning -- Follow Cube
+    -- then 
+    --     local new_pos = {}
+    --     local cube_pos = lua_table.Transform:GetPosition(Cube_ID)
 
-        new_pos[1] = cube_pos[1] + offset[1]
-        new_pos[2] = cube_pos[2] + offset[2]
-        new_pos[3] = cube_pos[3] + offset[3]
+    --     new_pos[1] = cube_pos[1] + offset[1]
+    --     new_pos[2] = cube_pos[2] + offset[2]
+    --     new_pos[3] = cube_pos[3] + offset[3]
       
-        lua_table.Transform:SetPosition(new_pos[1], new_pos[2], new_pos[3], lua_table.GameObjectFunctions:GetMyUID())
-    end
+    --     lua_table.Transform:SetPosition(new_pos[1], new_pos[2], new_pos[3], lua_table.GameObjectFunctions:GetMyUID())
+    -- end
 
-    if time > 1.76 and not music_played -- Accurate time when sound starts playing
-    then
+    -- if time > 1.76 and not music_played -- Accurate time when sound starts playing
+    -- then
 
-        lua_table.Audio:PlayAudioEvent("Play_lvl1_Intro_conversation_Cutscene")
-        music_played = true
-    end
+    --     lua_table.Audio:PlayAudioEvent("Play_lvl1_Intro_conversation_Cutscene")
+    --     music_played = true
+    -- end
 
-    if time > 31 and not start_motion
-    then
-        start_motion_time = time
-        position_z = lua_table.Transform:GetPosition(lua_table.GameObjectFunctions:GetMyUID())
-        start_motion = true 
-    end
+    -- if time > 31 and not start_motion
+    -- then
+    --     start_motion_time = time
+    --     position_z = lua_table.Transform:GetPosition(lua_table.GameObjectFunctions:GetMyUID())
+    --     start_motion = true 
+    -- end
 
-    if start_motion -- Zoom
-    then
-        local motion_time = time - start_motion_time
-        local value_ = motion_time / 5
+    -- if start_motion -- Zoom
+    -- then
+    --     local motion_time = time - start_motion_time
+    --     local value_ = motion_time / 5
         
-        local z = Lerp(position_z[3], -4000, value_)
+    --     local z = Lerp(position_z[3], -4000, value_)
 
-        lua_table.Transform:SetPosition(position_z[1], position_z[2], z, lua_table.GameObjectFunctions:GetMyUID())
-    end
+    --     lua_table.Transform:SetPosition(position_z[1], position_z[2], z, lua_table.GameObjectFunctions:GetMyUID())
+    -- end
 
-	 if time > 38 and next_scene == true
-    then
-        lua_table.Scene:LoadScene(lua_table.scene_uid)
-		next_scene = false
-    end
+	--  if time > 38 and next_scene == true
+    -- then
+    --     lua_table.Scene:LoadScene(lua_table.scene_uid)
+	-- 	next_scene = false
+    -- end
    
 end
 
