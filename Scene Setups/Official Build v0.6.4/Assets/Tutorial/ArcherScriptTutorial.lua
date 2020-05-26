d28b91bca958fa1936d168578f24954be8715f8f
Òscar Faura Gràcia <36189245+ofaura@users.noreply.github.com>
2020-05-27 01:18:47 +0200

    tutorial

---
+++ Scene Setups/Official Build v0.6.4/Assets/Tutorial/ArcherScriptTutorial.lua
@@ -0,0 +1,15 @@
+function GetTableArcherScriptTutorial()
+local lua_table = {}
+lua_table.System = Scripting.System()
+
+function lua_table:Awake()
+end
+
+function lua_table:Start()
+end
+
+function lua_table:Update()
+end
+
+return lua_table
+end
