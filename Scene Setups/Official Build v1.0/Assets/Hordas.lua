75477d2a9cd8ae8a22b8269a6b93dd2b0504f39e
DavidTello1 <davidtellop@gmail.com>
2020-06-07 18:32:40 +0200

    Hordes Script

--- Scene Setups/Official Build v1.0/Assets/Hordas.lua
+++ Scene Setups/Official Build v1.0/Assets/Hordas.lua
@@ -3,8 +3,9 @@ local lua_table = {}
 lua_table.System = Scripting.System()
 lua_table.GO = Scripting.GameObject()
 
+lua_table.begin = false
 lua_table.spawn_rate = 0
-
+ 
 lua_table.round0 = 0
 lua_table.round1 = 0
 lua_table.round2 = 0
@@ -15,6 +16,8 @@ lua_table.round6 = 0
 lua_table.round7 = 0
 lua_table.round8 = 0
 lua_table.round9 = 0
+
+lua_table.time = 0
 
 local round0_script = 0
 local round1_script = 0
@@ -30,42 +33,90 @@ local round9_script = 0
 local next_round = 0
 
 local function Spawn()
-    if time == lua_table.spawn_rate
+    if lua_table.time >= lua_table.spawn_rate
     then
-        if next_round = 0
+        if next_round == 0 and round0 > 0 --round 0
         then
-            round0_script:Spawn()
-        elseif next_round = 1
+            if round0_script.is_finished == false
+            then
+                round0_script:Spawn()
+            else
+                next_round = next_round + 1
+            end
+        elseif next_round == 1 and round1 > 0 --round 1
         then
-            round1_script:Spawn()
-        elseif next_round = 2
+            if round1_script.is_finished == false
+            then
+                round1_script:Spawn()
+            else
+                next_round = next_round + 1
+            end
+        elseif next_round == 2 and round2 > 0 --round 2
         then
-            round2_script:Spawn()
-        elseif next_round = 3
+            if round2_script.is_finished == false
+            then
+                round2_script:Spawn()
+            else
+                next_round = next_round + 1
+            end
+        elseif next_round == 3 and round3 > 0 --round 3
         then
-            round3_script:Spawn()
-        elseif next_round = 4
+            if round3_script.is_finished == false
+            then
+                round3_script:Spawn()
+            else
+                next_round = next_round + 1
+            end
+        elseif next_round == 4 and round4 > 0 --round 4
         then
-            round4_script:Spawn()
-        elseif next_round = 5
+            if round4_script.is_finished == false
+            then
+                round4_script:Spawn()
+            else
+                next_round = next_round + 1
+            end
+        elseif next_round == 5 and round5 > 0 --round 5
         then
-            round5_script:Spawn()
-        elseif next_round = 6
+            if round5_script.is_finished == false
+            then
+                round5_script:Spawn()
+            else
+                next_round = next_round + 1
+            end
+        elseif next_round == 6 and round6 > 0 --round 6
         then
-            round6_script:Spawn()
-        elseif next_round = 7
+            if round6_script.is_finished == false
+            then
+                round6_script:Spawn()
+            else
+                next_round = next_round + 1
+            end
+        elseif next_round == 7 and round7 > 0 --round 7
         then
-            round7_script:Spawn()
-        elseif next_round = 8
+            if round7_script.is_finished == false
+            then
+                round7_script:Spawn()
+            else
+                next_round = next_round + 1
+            end
+        elseif next_round == 8 and round8 > 0 --round 8
         then
-            round8_script:Spawn()
-        elseif next_round = 9
+            if round8_script.is_finished == false
+            then
+                round8_script:Spawn()
+            else
+                next_round = next_round + 1
+            end
+        elseif next_round == 9 and round9 > 0 --round 9
         then
-            round9_script:Spawn()
+            if round8_script.is_finished == false
+            then
+                round8_script:Spawn()
+            else
+                next_round = next_round + 1
+            end
         end
-
-        next_round = next_round + 1
-        time = 0
+        lua_table.time = 0
     end
 end
 
@@ -116,6 +167,11 @@ function lua_table:Start()
 end
 
 function lua_table:Update()
+    lua_table.time = lua_table.time + lua_table.System:DT()
+    if lua_table.begin == true
+    then
+        Spawn()
+    end
 end
 
 return lua_table
