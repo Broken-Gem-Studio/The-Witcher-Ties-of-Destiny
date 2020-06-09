8a08ba7b37f9e81ca576cef62921ac96f073e57a
Èric Canela <knela96@gmail.com>
2020-06-09 11:25:48 +0200

    chest + city prefab

--- Scene Setups/Official Build v1.2/Assets/New Folder 16/Props/Chest.lua
+++ Scene Setups/Official Build v1.2/Assets/New Folder 16/Props/Chest.lua
@@ -44,6 +44,8 @@ local barrel_particles_parent = 0
 local Sparkle = 0
 local timer = 0
 
+local spwn1, spwn2, spwn3, spwn4
+
 -- Main Code
 function lua_table:Awake ()
 	-- Get my own UID
@@ -53,6 +55,10 @@ function lua_table:Start ()
 	lua_table.myUID = lua_table.GameObjectFunctions:GetMyUID()
 	barrel_particles_parent = lua_table.GameObjectFunctions:FindChildGameObjectFromGO("Chest_Particles", lua_table.myUID)
 	Sparkle = lua_table.GameObjectFunctions:FindChildGameObjectFromGO("Sparkle", lua_table.myUID)
+	spwn1 = lua_table.GameObjectFunctions:FindChildGameObjectFromGO("spwn1", lua_table.myUID)
+	spwn2 = lua_table.GameObjectFunctions:FindChildGameObjectFromGO("spwn2", lua_table.myUID)
+	spwn3 = lua_table.GameObjectFunctions:FindChildGameObjectFromGO("spwn3", lua_table.myUID)
+	spwn4 = lua_table.GameObjectFunctions:FindChildGameObjectFromGO("spwn4", lua_table.myUID)
 	barrel_particles = lua_table.GameObjectFunctions:GetGOChilds(barrel_particles_parent)
 	lua_table.AnimationFunctions:PlayAnimation("Idle",30,lua_table.myUID)
 end
@@ -65,7 +71,7 @@ end
 
 local function GimbalLockWorkaroundY(param_rot_y)
 
-    if math.abs(lua_table.Transform:GetRotation(MyUID)[1]) == 180
+    if math.abs(lua_table.TransformFunctions:GetRotation(lua_table.myUID)[1]) == 180
     then
         if param_rot_y >= 0 then param_rot_y = 180 - param_rot_y
 		elseif param_rot_y < 0 then param_rot_y = -180 - param_rot_y
@@ -75,15 +81,20 @@ local function GimbalLockWorkaroundY(param_rot_y)
     return param_rot_y
 end
 
-local function newPotion(positionx,positiony,type)
-	local position = lua_table.TransformFunctions:GetPosition(lua_table.myUID)
+local function newPotion(type,spawner)
+	local position = 0
 	local rotation = lua_table.TransformFunctions:GetRotation(lua_table.myUID)
-
+	if spawner == 1 then
+		position = lua_table.TransformFunctions:GetPosition(spwn1)
+	elseif spawner == 2 then
+		position = lua_table.TransformFunctions:GetPosition(spwn2)
+	elseif spawner == 3 then
+		position = lua_table.TransformFunctions:GetPosition(spwn3)
+	elseif spawner == 4 then
+		position = lua_table.TransformFunctions:GetPosition(spwn4)
+	end
 
     local rot_fixed = GimbalLockWorkaroundY(rotation[2])
-
-    local X = math.sin(math.rad(rot_fixed))
-    local Z = math.cos(math.rad(rot_fixed))
 
 	local potion = 0		
 	local random = lua_table.SystemFunctions:RandomNumberInRange(1, 4)
@@ -91,12 +102,14 @@ local function newPotion(positionx,positiony,type)
 		random = type
 	end
 	if random < 2 then
-		potion = lua_table.Scenes:Instantiate(lua_table.health_potion, position[1] + X * positionx, position[2], position[3] + Z * positiony, 0, rot_fixed - 90, 0)
+		potion = lua_table.Scenes:Instantiate(lua_table.health_potion, position[1], position[2], position[3], 0, rot_fixed , 0)
 	elseif random < 3 then
-		potion = lua_table.Scenes:Instantiate(lua_table.stamina_potion, position[1] + X * positionx, position[2], position[3] + Z * positiony, 0, rot_fixed - 90, 0)
+		potion = lua_table.Scenes:Instantiate(lua_table.stamina_potion, position[1] , position[2], position[3], 0, rot_fixed , 0)
 	elseif random < 4 then
-		potion = lua_table.Scenes:Instantiate(lua_table.power_potion, position[1] + X * positionx, position[2], position[3] + Z * positiony, 0, rot_fixed - 90, 0)
+		potion = lua_table.Scenes:Instantiate(lua_table.power_potion, position[1] , position[2], position[3], 0, rot_fixed, 0)
 	end
+
+	return potion
 end
 
 function lua_table:Update ()
@@ -120,13 +133,10 @@ function lua_table:Update ()
 		if lua_table.SystemFunctions:GameTime() - timer > 0.7 then
 			lua_table.ParticlesFunctions:PlayParticleEmitter(Sparkle)
 			   
-			newPotion(-2,0.5,1)
-			--newPotion(-0.25,0.5,-1)
-			--newPotion(0.25,0.5,-1)
-			--newPotion(2,-0.5,1)
-
-			--potion = lua_table.Scenes:Instantiate(lua_table.health_potion, position[1]-2, position[2], position[3] - 0.5, rotation[1], rotation[2], rotation[3])
-			--potion = lua_table.Scenes:Instantiate(lua_table.health_potion, position[1]+2, position[2], position[3] - 0.5, rotation[1], rotation[2], rotation[3])
+			newPotion(-2,1)
+			newPotion(-0.5,2)
+			newPotion(0.5,3)
+			newPotion(2,4)
 			
 			current_state = state.OPENED
 		end
