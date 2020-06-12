ed71b72dd292ef288a472d9a20f77c5198c875a4
Victor Chen <36187875+Scarzard@users.noreply.github.com>
2020-06-12 16:13:20 +0200

    Outline to CityCinematic enemies

--- Scene Setups/Official Build v1.3/Assets/CinematicTutorial/Lumber_Alive.lua
+++ Scene Setups/Official Build v1.3/Assets/CinematicTutorial/Lumber_Alive.lua
@@ -1,12 +1,22 @@
 function GetTableLumber_Alive()
 local lua_table = {}
 lua_table.System = Scripting.System()
+lua_table.Transform = Scripting.Transform()
 lua_table.GameObject = Scripting.GameObject()
+lua_table.Audio = Scripting.Audio()
+lua_table.Scene = Scripting.Scenes()
+lua_table.Input = Scripting.Inputs()
+lua_table.UI = Scripting.Interface()
 lua_table.Animations = Scripting.Animations()
 
 local MyUID = 0
-local random = 0
-local play_anim = true
+local stand_timer = 0
+
+local attack = 0
+local swing_speed = 0
+
+local start_attack = false
+local attack_timer = 0
 
 function lua_table:Awake()
 end
@@ -16,14 +26,25 @@ function lua_table:Start()
 end
 
 function lua_table:Update()
-    random = math.random(1, 2)
+    if start_attack == false then 
 
-    if random == 1 and play_anim == true then
-        lua_table.Animations:PlayAnimation("Idle", 30.0, MyUID)
-        play_anim = false
-    elseif random == 2 and play_anim == true then
-        lua_table.Animations:PlayAnimation("Looking", 30.0, MyUID)
-        play_anim = false
+        attack = math.random(1, 2)
+        swing_speed = math.random(30, 50)
+        attack_timer = lua_table.System:GameTime() * 1000 
+
+        if attack == 1 then 
+            lua_table.Animations:PlayAnimation("Attack1", swing_speed, MyUID)
+        elseif attack == 2 then 
+            lua_table.Animations:PlayAnimation("Attack2", swing_speed, MyUID)
+        end
+
+        start_attack = true
+    end
+
+    if attack == 1 and attack_timer + 1000 <= lua_table.System:GameTime() * 1000 then 
+        start_attack = false
+    elseif attack == 2 and attack_timer + 1000 <= lua_table.System:GameTime() * 1000 then 
+        start_attack = false
     end
     
 end
