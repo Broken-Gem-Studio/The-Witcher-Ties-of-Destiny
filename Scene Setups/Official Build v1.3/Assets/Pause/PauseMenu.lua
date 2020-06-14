b11d95cd8e5a9862a17649f8a2458ad7132c5e5f
Dani Lorenzo <dlorenzolaguno17@gmail.com>
2020-06-14 14:24:08 +0200

    Improved pause prefab

--- Scene Setups/Official Build v1.3/Assets/Pause/PauseMenu.lua
+++ Scene Setups/Official Build v1.3/Assets/Pause/PauseMenu.lua
@@ -228,7 +228,7 @@ function lua_table:Update()
 			
 		elseif step == 2
 		then
-			if positionSum > 120 
+			if positionSum > 110 
 			then
 				increment = increment + 3
 				positionSum = positionSum - increment
@@ -239,7 +239,7 @@ function lua_table:Update()
 
 		elseif step == 3
 		then
-			if positionSum > -220 
+			if positionSum > -246 
 			then
 				increment = increment + 0.75
 				positionSum = positionSum - increment
